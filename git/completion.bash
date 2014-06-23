1811947b30daa355b06e1e5043ad3045915cb82b
Su Zhang <me@zhang.su>
2014-06-22 21:21:42 -0400

    Remove unnecessary git auto completion
    There is nothing to complete at the `git submodule update` level.

--- git/completion.bash
+++ git/completion.bash
@@ -41,7 +41,6 @@ __git_complete grm _git_rm
 __git_complete grmt _git_remote
 __git_complete gsh _git_show
 __git_complete gsm _git_submodule
-__git_complete gsmu _git_submodule
 __git_complete gst _git_stash
 __git_complete gstc _git_stash
 __git_complete gstd _git_stash
@@ -49,4 +48,3 @@ __git_complete gstk _git_stash
 __git_complete gstl _git_stash
 __git_complete gstp _git_stash
 __git_complete gsts _git_stash
-
